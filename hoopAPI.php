4a3fa547955c348f1d6ae6ede1fd4d70f2ea96cc
boitymtsatse <boitymtsatse@gmail.com>
2024-05-17 13:07:47 +0200

    committing changes to branch

--- hoopAPI.php
+++ hoopAPI.php
@@ -2,7 +2,7 @@
     echo "In Class!";
 
     class Hoop{
-
+        protected $con;
         public static function instance() {
             static $instance = null; 
             if($instance === null)
@@ -29,13 +29,35 @@
 
         public function handleRequest(){
 
-            $request_body = file_get_contents('php://input');
-            $data = json_decode($request_body,true);
-            $hoop = Hoop::instance();
+            // $request_body = file_get_contents('php://input');
+            // $data = json_decode($request_body,true);
+            // $hoop = Hoop::instance();
             
-            if($_SERVER["REQUEST_METHOD"] == "POST"){
+            if ($_SERVER["REQUEST_METHOD"] === "POST") {
+                $reqbody = json_decode(file_get_contents('php://input'), true);
+                $type = $reqbody["type"];
+                if (!isset($type)) {
+                    echo "Error: No Type";
+                    return;
+                }
+    
+                if ($type === "SignUp") {
+                    $this->signUp();
+                }
+                if($type==="Login") 
+                {
+                    $this->login();
+                }
+                if($type==="GetAllTitles") 
+                {
+                    $this->getTitles();
+                }
+
+
             }
+            
         }
+        
 
         public function signUp(){
 
@@ -44,9 +66,28 @@
         public function login(){
             
         }
+        public function getTitles()
+        {
+
+        }
 
         
     }
 
     $hoop = Hoop::instance();
+
+class Response{
+
+    public $status;
+    public $timestamp;
+    public $data;
+
+
+    function __construct($status, $timestamp, $data)
+    {
+        $this->status = $status;
+        $this->timestamp = $timestamp;
+        $this->data = $data;
+    }
+}
 ?>
