ccc2599ba48fbd4ca2f2295816b7b1fa3a24052c
Driya Govender <u23535793@tuks.co.za>
2024-05-17 11:57:15 +0200

    Function Headers

--- hoopAPI.php
+++ hoopAPI.php
@@ -1,21 +1,26 @@
 <?php
     echo "In Class!";
 
-    class Hoop{
+    class Hoop
+    {
 
-        public static function instance() {
+        public static function instance() 
+        {
             static $instance = null; 
             if($instance === null)
                 $instance = new Hoop();
             return $instance; 
         }
 
-        private function __construct() {
+        private function __construct() 
+        {
             $con = new mysqli("wheatley.cs.up.ac.za","u23535793","XSJE56JRFVO4MIIHAMP4QGSUX7M32JED","u23535793_HOOP");
-            if ($con->connect_error) {
+            if ($con->connect_error) 
+            {
                 die("Connection failed: ".$con->connect_error);
             }
-            else{
+            else
+            {
                 $this->con = $con;
                 echo "Connected!";
             }
@@ -23,25 +28,46 @@
             return $con;
         }
 
-        public function __destruct() { 
+        public function __destruct() 
+        { 
             mysqli_close($this->con);
         }
 
-        public function handleRequest(){
+        public function handleRequest()
+        {
 
             $request_body = file_get_contents('php://input');
             $data = json_decode($request_body,true);
             $hoop = Hoop::instance();
             
-            if($_SERVER["REQUEST_METHOD"] == "POST"){
+            if($_SERVER["REQUEST_METHOD"] == "POST")
+            {
+
             }
         }
 
-        public function signUp(){
+        public function signUp()
+        {
 
         }
 
-        public function login(){
+        public function login()
+        {
+            
+        }
+
+        public function setUserPref()
+        {
+
+        }
+
+        public function getAllTitle()
+        {
+
+        }
+
+        public function search()
+        {
             
         }
 
