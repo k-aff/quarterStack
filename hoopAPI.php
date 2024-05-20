12f98a40cf3c26d13dbce06f391e6500309d4a68
Karabo Serothoane <u23543087@tuks.co.za>
2024-05-20 23:56:28 +0200

    GetTitlesDone

--- hoopAPI.php
+++ hoopAPI.php
@@ -1,8 +1,10 @@
 <?php
     echo "In Class!";
+    session_start();
 
     class Hoop
     {
+
         protected $con;
         public static function instance() 
         {
@@ -53,9 +55,8 @@
                 }
                 if($type==="getAllTitles") 
                 {
-                    $this->getAllTitles();
+                    $this->getAllTitles($reqbody);
                 }
-
 
             }
             
@@ -77,9 +78,95 @@
 
         }
 
-        public function getAllTitle()
+        public function getAllTitles($data)
         {
+            // $email = $_SESSION['email']; //use id instead?  $id = $_SESSION['id']
 
+            $sqlcheckpref = "SELECT * FROM user_preferences WHERE user_id='$id'";
+            $result = $this->con->query($sqlcheckpref);
+            $sqlReturnTitles = "SELECT * FROM title";
+
+            if($result)
+            {
+                $pref = $result->fetch_assoc();
+                $type = $pref["type"];
+                $genre1 = $pref["genre_id_1"];
+                $genre2 = $pref["genre_id_2"];
+                $genre3 = $pref["genre_id_3"];
+
+                //Assuming users set atleast one genre
+                if($type!==null)
+                    $sqlReturnTitles = $sqlReturnTitles. " WHERE type=". $type;
+                if($genre1!=null)
+                    $sqlReturnTitles = $sqlReturnTitles. " AND genre_id=". $genre1;
+                if($genre2!=null)
+                    $sqlReturnTitles = $sqlReturnTitles. " OR genre_id=". $genre2;
+                if($genre3!=null)
+                    $sqlReturnTitles = $sqlReturnTitles. " OR genre_id=". $genre3;
+            }
+            else
+            {
+                echo "No preferences set";
+            }
+
+            $result = $this->con->query($sqlReturnTitles);
+
+            if($result)
+            {
+                $titlesToRet = array();
+
+                //creating an associative array of what to return for each tuple returned from database
+
+                while($title = $result->fetch_assoc()){
+
+                    $genre_id = $title["genre_id"];
+                    $sql1 = "SELECT genre from genre WHERE genre_id='$genre_id'";
+                    $result1 = $this->con->query($sql1);
+                    $titleGenre = $result1->fetch_assoc();
+                    $genre = $titleGenre["genre"];
+
+                    $data = array();
+                    $data['title'] = $title["title"];
+                    $data['type'] = $title["type"];
+                    $data['age_cert'] = $title["age_cert"];
+                    $data['plot'] = $title["plot_summary"];
+                    $data['languages'] = (explode(', ', $title["language"]))[0];
+                    $data['release date'] = $title["release_date"];
+                    $data['genre'] = $genre;
+                    $data['producer'] = (explode(', ', $title["crew"]))[0];
+                    $data['cast'] = $title["cast"];
+                    $data['image'] = $title["image"];
+
+                    //obtaining additonal movie or series data 
+                    if($title["type"] == 'M')
+                    {
+                        $title_id = $title["title_id"];
+                        $sql2 = "SELECT runtime FROM movie WHERE title_id = '$title_id'";
+                        $result2 = $this->con->query($sql2);
+                        $titleRuntime = $result2->fetch_assoc();
+                        $data['runtime'] = $titleRuntime["runtime"];
+                    }
+                    else if($title["type"] == 'S'){ 
+
+                        $title_id = $title["title_id"];
+                        $sql2 = "SELECT * FROM tv_series WHERE title_id = '$title_id'";
+                        $result2 = $this->con->query($sql2);
+                        $titleEps = $result2->fetch_assoc();
+                        $data['number of seasons'] = $titleEps["no_of_seasons"];
+                        $data['total of episodes'] = $titleEps["no_of_episodes"];
+
+                    }
+
+                    $titlesToRet[] = $data;
+                }
+
+                //creating JSON response
+                echo json_encode(new Response("success", time(), $titlesToRet));
+
+            }
+            else{
+                echo json_encode(new Response("success", time(), "Error occured"));
+            }
         }
 
         public function search()
@@ -91,6 +178,7 @@
     }
 
     $hoop = Hoop::instance();
+    $hoop->handleRequest();
 
 class Response{
 
