425a662ad98fa6999d72b20de4f5df77e94a77dc
Driya Govender <u23535793@tuks.co.za>
2024-05-26 11:37:45 +0200

    added country drop down to signUp and updated css

---
+++ js/signup.js
@@ -0,0 +1,13 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Select the form
+//     const form = document.getElementById('signup-form');
+
+//     // Add event listener for form submission
+//     form.addEventListener('submit', function(event) {
+//         // Prevent the default form submission behavior
+//         event.preventDefault();
+
+//         console.log("hello")
+
+//     });
+// });
