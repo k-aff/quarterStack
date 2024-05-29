f0f9ef58bd2eb840d24997ea6a3d1f748e2c2add
Michelle Njoroge <micheyn83uni@gmail.com>
2024-05-29 21:23:53 +0200

    intro loads to homepage when done

--- js/intro.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    setTimeout(() => {
-        window.location.href = 'homepage.html'; // Redirect to homepage after 4 seconds
-    }, 4000); // Adjusted timeout to 4000 milliseconds (4 seconds)
-});
