ca3538cc7b2964034e73d9dd53de19f9a52cdb73
Driya Govender <u23535793@tuks.co.za>
2024-05-29 20:09:44 +0200

    login completed

--- js/login.js
+++ js/login.js
@@ -1,38 +1,50 @@
-function Login(event) {
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.getElementById("loginForm");
+
+    form.addEventListener("submit", function(event) {
     event.preventDefault();
   
     //make use of user ID or session id
-    const api = "hoopAPI.php";
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-  
-    const requestData = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        type: "login",
-        email: email,
-        password: password,
-      }),
-    };
-  
-    fetch(api, requestData)
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.status == "Error") {
-          //if error display error message
-        } else {
-          console.log("Success:", data);
-          window.location.href = "homepage.html";
+
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; // email pattern
+    if (!emailPattern.test(email)) {
+        alert('Email is incorrect');
+        return; 
+    }
+
+    //check if password is valid
+    const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()-_=+{};:,<.>]).{8,}$/; // password pattern
+    if (!passwordPattern.test(password)) {
+        alert('Password is incorrect');
+        return; 
+    }
+
+    var req = new XMLHttpRequest();  
+    req.onreadystatechange = function()
+    {
+        if(req.readyState == 4 && req.status == 200)
+        {
+            var login = JSON.parse(req.responseText);
+            console.log(login);
+
+            if (login.status == "Error")
+                alert('Invalid credentials'); 
+            else 
+                window.location.replace('homepage.html');
         }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        // Handle error
-      });
-  }
-  document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("loginForm").addEventListener("submit", Login);
-  });
+    }
+    req.open("POST", "http://localhost/quarterStack/hoopAPI.php", false); 
+    req.setRequestHeader("Content-Type", "application/json");
+
+    const request = 
+    {
+        "type": "login",
+        "email":  email,
+        "password":  password
+    };
+
+    req.send(JSON.stringify(request));       
+}); 
+});
